7ede4ccc2910673dbba55b79bdb51b25a59383eb
Thomas van der Hoeven <thomas@fourdigits.nl>
2021-06-01 14:00:09 +0200

    Remove files

--- src/wagtail-xliff-translation/xliff/test/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class XliffTestAppConfig(AppConfig):
-    label = "xliff_test"
-    name = "xliff.test"
-    verbose_name = "Xliff import/export testapp"
--- src/wagtail-xliff-translation/xliff/test/factories/__init__.py
+++
@@ -1,1 +0,0 @@
-from .page_factories import *  # noqa
