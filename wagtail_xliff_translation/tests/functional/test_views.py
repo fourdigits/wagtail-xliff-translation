ceb64b1a2f4d9e840cc60d409c2eceafa96a3e2f
Coen van der Kamp <coen@fourdigits.nl>
2021-06-03 15:45:03 +0200

    Fix test_upload_view_post

--- wagtail_xliff_translation/tests/functional/test_views.py
+++ wagtail_xliff_translation/tests/functional/test_views.py
@@ -31,24 +31,12 @@ def test_upload_view_get(admin_client, page_factory):
     resp = admin_client.get(reverse("xliff:upload", kwargs={"page_id": page.pk}))
     assert isinstance(resp.context["form"], ImportForm)
 
-@pytest.mark.skip(reason="Uploading needs to be fixed")
+
 def test_upload_view_post(
     admin_client, page_factory, site_factory, locale_factory
 ):
-    de_lang = locale_factory(language_code="de")
+    locale_factory(language_code="de")
     root_page = Site.objects.first().root_page
-    root_page.copy_for_translation(de_lang)
-    # parent = page_factory(parent=site.root_page)
-    # parent = []
-    # page = page_factory(parent=parent)
-    # parent.copy_for_translation(de_lang)
-    # german_parent = parent.copy_for_translation(de_lang)
-    # upload_file = open(
-    #     os.path.join(
-    #         settings.BASE_DIR, "xliff/tests/data/xliff_translated/zgpage.xliff"
-    #     ),
-    #     "rb",
-    # )
     upload_file = open(
         os.path.join(
             settings.BASE_DIR, "test_app/data/xliff_translated/zgpage.xliff"
@@ -60,9 +48,8 @@ def test_upload_view_post(
     xml_tree = ET.fromstring(xml_string)
     for file_element in xml_tree.findall("file"):
         file_element.set("id", PageHelper.get_object_id(root_page))
-    breakpoint()
     xml_string = ET.tostring(xml_tree, encoding="unicode")
-    # ET removes onloaded namespaces
+    # ET removes not loaded namespaces
     xml_string = xml_string.replace("ns0", "mda")
 
     resp = admin_client.post(
@@ -72,7 +59,6 @@ def test_upload_view_post(
             "update_target_page": True,
             "update_subtree": True,
             "create_pages": True,
-            "parent_page": root_page.get_translation(de_lang).id,
         },
     )
     assert resp.url == reverse("wagtailadmin_explore", args=[root_page.pk])
