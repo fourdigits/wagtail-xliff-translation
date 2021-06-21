510dea07b96dcdaebeff521afba0fb0214852b7d
Coen van der Kamp <coen@fourdigits.nl>
2021-06-21 21:17:02 +0200

    Update secret key to secret

--- test_app/settings.py
+++ test_app/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "n_o6888120pwd%y!7l_icn(rzmmsl(6(1%@e5%!ey1(2feym!o"
+SECRET_KEY = "secret"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
